38c5bcb5f75ef7da0dd2e4c11b37f7d42df46bfb
Enguerran POULAIN <contact@epoulain.fr>
2016-06-13 23:58:55 +0200

    Part 3-4a, 3-4b
    Former-commit-id: 30b0e3d6fea266f58902d11805ee4ef0680de1b0
    Former-commit-id: 28ab7f45307e0e6a6dfabb09e40a504f354cb323

--- scripts/part3-4b.js
+++ scripts/part3-4b.js
@@ -4,20 +4,9 @@ const MongoClient   = require('mongodb').MongoClient;
 
 MongoClient.connect('mongodb://localhost:27017/music', (err, db) => {
     const UserCol = db.collection('users');
-    const SongCol = db.collection('songs');
 
-    UserCol.find({}).forEach((user) => {
-        let totalFavoriteSongs = Math.floor(Math.random() * 11);
-
-        SongCol.aggregate([{$sample: {total: totalFavoriteSongs}}], (aggregation) => {
-            
-            })
-        });
-    });
-
-    db.collection('users').insertMany(users)
+    UserCol.find({"favoriteSongs.artist": {$in: ["Coldplay"]}}).toArray()
         .then(users => { console.log(users); })
         .catch(err => { console.log(err); })
         .then(() => db.close())
-    ;
 });
